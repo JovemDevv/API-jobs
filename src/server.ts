5cbe259f77ddf6686a7a6b7057563b226a8868f1
JovemDevv <anacarolinesan18@gmail.com>
2024-02-24 10:32:55 -0300

    criando controladores de rota

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-
-import { Candidate } from "./models/candidate";
+import { router } from "./routes";
 
 const app = express();
 
-const router = express.Router();
-
-router.get("/", (req, res) => res.json({ hello: "Hello" }));
-
-router.get("/candidates", async (req, res) => {
-  const candidates = await Candidate.findAll();
-  return res.json(candidates);
-});
+app.use(express.json());
 
 app.use(router);
 
 const PORT = process.env.PORT || 3333;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log("Started!");
+});
