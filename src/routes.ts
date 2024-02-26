cdb5e55622ffb3b7b743f73f15cb55ff6a2eb2a2
JovemDevv <anacarolinesan18@gmail.com>
2024-02-26 09:25:04 -0300

    Criando tabela de empresas

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { candidatesController } from "./controllers/candidates-controller";
+import { companiesController } from "./controllers/companies-controller";
+
 const router = express.Router();
 
 router.get("/", (req, res) => res.json({ hello: "Hello, world!" }));
@@ -9,5 +11,7 @@ router.post("/candidates", candidatesController.save);
 router.get("/candidates/:id", candidatesController.show);
 router.put("/candidates/:id", candidatesController.update);
 router.delete("/candidates/:id", candidatesController.delete);
+
+router.get("/companies", companiesController.index);
 
 export { router };
